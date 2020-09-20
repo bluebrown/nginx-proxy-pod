7566ce46dc597028bac6a1d997e828244aeb198c
Nico <nico-braun@live.de>
2020-09-20 23:57:13 +0000

    add post endpoint to sample api

--- python-api/app/main.py
+++ python-api/app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-import motor.motor_asyncio
-
 from fastapi.responses import Response
+from motor.motor_asyncio import AsyncIOMotorClient
 from bson.json_util import dumps
-
 
 class MongoResponse(Response):
     def __init__(self, content, *args, **kwargs):
@@ -14,20 +12,22 @@ class MongoResponse(Response):
             **kwargs,
         )
 
-
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://mongoadmin:secret@localhost:27017/?authSource=admin')
+client = AsyncIOMotorClient(
+    'mongodb://mongoadmin:secret@localhost:27017/?authSource=admin'
+)
 db = client.sampleDB
-
 app = FastAPI()
 
-
 @app.get("/ping")
-def root():
+def health_check():
     return {"status": "alive"}
 
-
 @app.get("/mongo")
-async def mongo():
+async def get_item():
     result = await db.posts.find_one()
     return MongoResponse(result)
 
+@app.post("/mongo")
+async def add_item(body: dict):
+    result = await db.posts.insert_one(body)
+    return {"msg": "ok"}
